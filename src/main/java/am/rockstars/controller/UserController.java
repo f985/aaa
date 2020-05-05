6105c52b5033db910bbb3f04e2e726467351055e
Artur Vahanyan <arthur.vahanyan@sfl.am>
2020-05-05 22:16:56 +0400

    added endpoint to get user info by jwt

--- src/main/java/am/rockstars/controller/UserController.java
+++ src/main/java/am/rockstars/controller/UserController.java
@@ -1,8 +1,11 @@
 package am.rockstars.controller;
 
 import am.rockstars.dto.CreateUserRequest;
+import am.rockstars.dto.UserBean;
 import am.rockstars.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,9 +23,9 @@ public class UserController {
         return ResponseEntity.ok(userService.createUser(user));
     }
 
-    @GetMapping
-    public ResponseEntity<?> getAllUsers() {
-        return ResponseEntity.ok(userService.getAllUsers());
+    @GetMapping(value = "/current-user")
+    public ResponseEntity<?> getCurrentUser(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return ResponseEntity.ok(userService.getUserByEmail(auth.getName()));
     }
-
 }
