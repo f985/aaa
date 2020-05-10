d91172b710384063f174bb44bc5b9d89990268ea
Aramayis <armen.181@gmail.com>
2020-05-10 14:45:52 +0400

    added toString

--- src/main/java/am/rockstars/dto/EditUserProfileRequest.java
+++ src/main/java/am/rockstars/dto/EditUserProfileRequest.java
@@ -2,11 +2,13 @@ package am.rockstars.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@ToString
 public class EditUserProfileRequest {
 
     private String email;
--- src/main/java/am/rockstars/dto/UserPasswordResetRequest.java
+++ src/main/java/am/rockstars/dto/UserPasswordResetRequest.java
@@ -2,9 +2,11 @@ package am.rockstars.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class UserPasswordResetRequest {
     private String key;
     private String password;
