6cf333e4842f314acc7aa88c4fd2fcc93e279a53
Sergey <serg.yer@gmail.com>
2020-06-06 15:43:23 +0400

    testing

--- src/main/java/am/rockstars/controller/ProductController.java
+++ src/main/java/am/rockstars/controller/ProductController.java
@@ -23,7 +23,7 @@ import javax.validation.Valid;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/products")
-@Api(value = "Products controller asdasdasdasdasdas")
+@Api
 public class ProductController {
 
     private final ProductService productService;
